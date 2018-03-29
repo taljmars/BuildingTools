05853b59e719874904ef648c40a299ef3e48f75b
taljmars <taljmars@gmail.com>
2018-03-29 03:18:31 +0300

    .

--- DroneProject/build.bat
+++ DroneProject/build.bat
@@ -9,7 +9,7 @@ shift
 echo --------------- Windows 64 - Lite --------------
 del %TAKEDIR%\Windows64-lite\*.zip
 echo f | xcopy /f /y %TAKEDIR%\..\DroneGCS\ClientInstallation\Windows\Console-*-win-x64.zip %TAKEDIR%\Windows64-lite\Console-1.%BLDVERSION%-win-x64.zip
-echo f | xcopy /f /y %TAKEDIR%\..\DroneServer\ServerInstallation\Windows64-lite\ServerCore-*-win-x64.zip %TAKEDIR%\Windows64-lite\ServerCore-1.%BLDVERSION%-win-x64.zip
+echo f | xcopy /f /y %TAKEDIR%\..\DroneServer\ServerInstallation\Windows64-lite\ServerCore-*-win-x64-lite.zip %TAKEDIR%\Windows64-lite\ServerCore-1.%BLDVERSION%-win-x64-lite.zip
 
 ::echo --------------- Linux --------------
 ::del %TAKEDIR%\Linux32\*.tar
