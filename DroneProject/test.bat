194cea6cc94ee551ade51acceee01752b80e7975
taljmars <taljmars@gmail.com>
2018-03-29 04:31:08 +0300

    .

--- DroneProject/test.bat
+++ DroneProject/test.bat
@@ -18,6 +18,9 @@ shift
 @ECHO --------------- Windows 64 Tester --------------
 @ECHO f | xcopy /f /y %TAKEDIR%\Windows64-lite\* %TESTPATH%\
 @CD %TESTPATH%
+@ECHO INSTALLING
 @CALL install.bat
+@ECHO RUNNING
+@CALL run.bat
 
 @ECHO Done
